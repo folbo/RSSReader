2ddc42fe9efc6392d0d7b2cf221f4455fa1a1317
Sylwekqaz <sylwekqaz19@gmail.com>
2015-05-30 18:59:17 +0200

    nowe modele

--- ATOMUltimate/Model/Atom.cs
+++ ATOMUltimate/Model/Atom.cs
@@ -8,9 +8,47 @@ using System.Xml.Serialization;
 
 namespace ATOMUltimate.Model
 {
-    [XmlRoot("feed")]
+    [XmlRoot(ElementName = "feed")]
     public class Atom
     {
+        [XmlElement(ElementName = "title")]
         public string Title { get; set; }
+        [XmlElement(ElementName = "link")]
+        public Link Link { get; set; }
+        [XmlElement(ElementName = "description")]
+        public string Description { get; set; }
+        [XmlElement(ElementName = "lastBuildDate")]
+        public DateTime LastBuildDate { get; set; }
+        [XmlElement(ElementName = "updated")]
+        public DateTime Updated { get; set; }
+        [XmlElement(ElementName = "language")]
+        public string Language { get; set; }
+        [XmlElement(ElementName = "generator")]
+        public string Generator { get; set; }
+        [XmlElement(ElementName = "item")]
+        public Item[] Items { get; set; }
+
+        public Author Author { get; set; }
+
+    }
+
+    public class Link
+    {
+        [XmlAttribute(AttributeName = "href")]
+        public string Url { get; set; }
+        [XmlText]
+        public string Name { get; set; }
+    }
+
+    public class Author
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class Item
+    {
+        public string Title { get; set; }
+        public Link Link { get; set; }
     }
 }
--- XmlReaderTester/Program.cs
+++ XmlReaderTester/Program.cs
@@ -1,23 +1,67 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Xml.Serialization;
 using ATOMUltimate.Model;
 
 namespace XmlReaderTester
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            XmlSerializer serializer = new XmlSerializer(typeof (Atom));
+            //Serialize();
+            Deserialize();
+        }
+
+        private static void Serialize()
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof (Atom), @"http://www.w3.org/2005/Atom");
+
 
             var atom = new Atom()
             {
-                Title = "asd"
+                Title = "asd",
+                Link = new Link()
+                {
+                    Name = "Google",
+                    Url = @"http://www.google.pl"
+                },
+                Author = new Author()
+                {
+                    Name = "STurski",
+                    Email = "qaz@mailnesia.com"
+                },
+                Description = "opis",
+                Generator = "Renia lewa i prawa",
+                Language = "pl-PL",
+                LastBuildDate = DateTime.Today,
+                Updated = DateTime.Today,
+                Items = new[]
+                {
+                    new Item() {Title = "pierwszy"},
+                    new Item() {Title = "drugi"},
+                }
             };
 
 
             serializer.Serialize(Console.Out, atom);
+
+            Console.ReadKey();
+        }
+
+
+        private static void Deserialize()
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof (Atom), @"http://www.w3.org/2005/Atom");
+
+
+            Stream stream = new FileStream(@"..\..\AtomExample.xml", FileMode.Open);
+
+            var atom = serializer.Deserialize(stream);
+
 
             Console.ReadKey();
         }
